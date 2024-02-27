10fcb5f39dd09f65e42dfb21a1e9fdfc3fd3d1a1
Kaue Santana <kauesantana_13@hotmail.com>
2024-02-26 22:15:47 -0300

    Aula 26.02

--- Aula 26.02/variaveis.kt
+++ Aula 26.02/variaveis.kt
@@ -6,14 +6,51 @@
 
 // }
 
+// fun main() {
+//     var numeroInteiro: Int = 10
+//     var numeroDecimal: Double = 3.14
+//     var texto: String =  "Olá Mundo!"
+//     var status: Boolean = true
+    
+//     println("Número Intero: $numeroInteiro")
+//     println("Número Decimal: $numeroDecimal")
+//     println("Texto: $texto")
+    
+// }
+
+
+// Variáveis com tipo de dados
+// Char, BYTE, Short, Long
+
+// fun main () {
+//     val numeroInteiro: Int = 10
+//     val numeroLong: Long = 10000000
+//     val numeroDecimal: Double = 211.14
+//     val caractere: Char = 'V'
+//     val texto: String = "Kotlin também serve para back end"
+//     val verdadeiro: Boolean = true
+
+//     println("Interio $numeroInteiro")
+//     println("Long $numeroLong")
+//     println("Deciml $numeroDecimal")
+//     println("Caractere $caractere")
+//     println("Texto $texto")
+//     println("Verdadeiro $verdadeiro")
+
+// }
+
+
+
+// Multabilidde e Imutabilidade no contexto de var e val
+
 fun main() {
-    var numeroInteiro: Int = 10
-    var numeroDecimal: Double = 3.14
-    var texto: String =  "Olá Mundo!"
-    var status: Boolean = true
-    
-    println("Número Intero: $numeroInteiro")
-    println("Número Decimal: $numeroDecimal")
-    println("Texto: $texto")
-    
+
+    val imutavel: Int = 5 // imutavel
+    var mutavel: Int = 10 // mutable
+
+    mutavel = 15 // ok
+    imutavel = 20 // Erro! Não pode atribuir um valor a uma variavel declarada como imutavel
+
+    println("Antes da alteração o valor é $imutavel")
+    println("Depois da alteração o valor é $mutavel")
 }
