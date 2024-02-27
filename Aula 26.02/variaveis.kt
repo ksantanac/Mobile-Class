db7761d3f05bd46c12c50367b7c99c2cdfa11c54
Kaue Santana <kauesantana_13@hotmail.com>
2024-02-26 21:48:19 -0300

    Aula 26.02

---
+++ Aula 26.02/variaveis.kt
@@ -0,0 +1,19 @@
+// fun main () {
+
+//     val nome = "Kaue" // variavel tipo string  // mutavel
+//     var idade = 41    // variavel do tipo int     // não mutavel
+//     println("Nome: $nome, Idade: $idade")
+
+// }
+
+fun main() {
+    var numeroInteiro: Int = 10
+    var numeroDecimal: Double = 3.14
+    var texto: String =  "Olá Mundo!"
+    var status: Boolean = true
+    
+    println("Número Intero: $numeroInteiro")
+    println("Número Decimal: $numeroDecimal")
+    println("Texto: $texto")
+    
+}
