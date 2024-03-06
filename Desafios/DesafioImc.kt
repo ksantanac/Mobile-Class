9aced8e70f582b552197ad254045c9f6b06b9ef2
Kaue Santana <kauesantana_13@hotmail.com>
2024-03-06 14:50:47 -0300

    Desafio IMC

--- Desafios/DesafioImc.kt
+++ Desafios/DesafioImc.kt
@@ -1,5 +1,5 @@
 // 1. Criar um objeto Scanner para ler a entrada do usuário.
-// 2. Definir uma função chamada coletarDados() que solicite e leia o peso e aaltura do usuário.
+// 2. Definir uma função chamada coletarDados() que solicite e leia o peso e altura do usuário.
 // 3. Calcular o IMC utilizando a fórmula IMC = peso / (altura * altura).
 
 // Utilizar uma estrutura when para determinar a categoria do IMC com basenos seguintes critérios:
@@ -16,23 +16,59 @@
 // Para executar o código Kotlin e ver a saída, você precisará de um ambiente de desenvolvimento ou 
 // compilador Kotlin instalado em seu sistema. Aqui estão os passos básicos para executar ocódigo:
 
-fun main() {
-    coletarDados()
-}
 
-fun coletarDados() {
+fun coletarDados(): Pair<Double, Double> {
+
+    println("-----------------------------")
+    println("       Calculo IMC      ")
+    println("-----------------------------")
+
     println("Digite seu peso em KG: ")
-    val peso = readLine()!!.toDouble() 
+    val peso = readLine()!!.toDouble()
+
+    println("")
 
     println("Digite sua altura: ")
-    val altura = readLine()!!.toDouble() 
+    val altura = readLine()!!.toDouble()
 
+    println("")
 
-    println(peso)
+    return peso to altura
 }
 
-fun main() {
-    val peso = coletarDados()
-    println(peso)
 
+fun classificarIMC(imc: Double) {
+    val peso = when {
+        imc < 18.5 -> "Excesso de Magreza"
+        imc < 25 -> "Peso Normal"
+        imc < 30 -> "Excesso de Peso"
+        imc < 35 -> "Obesidade - Grau I"
+        imc < 40 -> "Obesidade - Grau II"
+        else -> "Obesidade - Grau III"
+    }
+    println("-----------------------------")
+    println("Seu IMC é: $imc")
+    println("Você está com a condição de: $peso")
+    println("-----------------------------")
+}
+
+
+
+fun main() {
+
+    while (true) {
+
+        val (peso, altura) = coletarDados()
+
+        val calculoImc = peso / (altura * altura)
+
+        classificarIMC(calculoImc)
+
+        println("Deseja calcular o IMC novamente? (S/N)")
+        val resposta = readLine()?.uppercase()
+
+        if (resposta != "S") {
+            break
+        }
+    }
 }
